feebd141d0c9cf3772bf2bfaa92be7c064903076
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-12 15:14:57 +0200

    Radium example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Person from './Person/Person';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
+import Person from './Person/Person';
 
 class App extends Component {
 
@@ -57,10 +58,16 @@ class App extends Component {
 
   render() { 
     const style = {
-      backgroundColor:'white',
+      backgroundColor:'green',
+      color:'white',
       font:'inherit',
       border: '1px solid blue',
-      cursor: 'pointer'
+      padding:'8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color:'black'
+      }
     }
 
     let persons = null;
@@ -91,21 +98,38 @@ class App extends Component {
             age={this.state.persons[2].age} 
           /> */}
         </div>
-      )
+      );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color:'black'
+      }
     }
 
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        {/* <button style={style} onClick={this.switchNameHandler.bind(this, 'Maksimilijan')}>Switch Name</button>
-        <button style={style} onClick={() => this.switchNameHandler('Metallica')}>Switch Name 2</button> */}
-        <button style={style} onClick={this.togglePersonsHandler}>Toggle persons</button>
-        
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          {/* <button style={style} onClick={this.switchNameHandler.bind(this, 'Maksimilijan')}>Switch Name</button>
+          <button style={style} onClick={() => this.switchNameHandler('Metallica')}>Switch Name 2</button> */}
+          <button style={style} onClick={this.togglePersonsHandler}>Toggle persons</button>
+          
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Radium from 'radium';
 import './Person.css'
+
+// Radium is not working for @media, need to check what is issue
 
 const person = (props) => {
   // const {name,age,click,change} = props;
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
       <div>{props.children}</div>
       <input onChange={props.change} type="text" value={props.name} />
@@ -12,4 +20,4 @@ const person = (props) => {
   )
 };
 
-export default person;
+export default Radium(person);
