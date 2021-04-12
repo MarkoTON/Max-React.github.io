8dae4b78d9a4f60c417412a9fca6a53bb108268c
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-12 16:03:46 +0200

    config file changes

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,36 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
-// import './Person.css'
 
-// Radium is not working for @media, need to check what is issue
+import classes from './Person.css';
 
-const StyleDiv = styled.div`
-  width: 60%;
-  margin: 1rem auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px){
-    width 450px;
-  }
-`;
-
-const person = (props) => {
-  // const {name,age,click,change} = props;
-  // const style = {
-  //   '@media (min-width: 500px)': {
-  //     width: '450px'
-  //   }
-  // }
+const person = props => {
   return (
-    <StyleDiv>
-      <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
-      <div>{props.children}</div>
-      <input onChange={props.change} type="text" value={props.name} />
-    </StyleDiv>
-  )
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
 export default person;
