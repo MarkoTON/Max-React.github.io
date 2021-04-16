d12f65527e2b7ac9cdec8fa04ab19adca3ac7d0a
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-16 11:38:00 +0200

    redux assignment 1 solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import reducer from './store/reducer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,26 +1,27 @@
-import * as actionTypes from './actions';
-
 const initialState = {
-    persons: []
+    counter: 0
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
-        case actionTypes.ADD_PERSON:
-            const newPerson = {
-                id: Math.random(), // not really unique but good enough here!
-                name: action.personData.name,
-                age: action.personData.age
-            }
-            return {
-                ...state,
-                persons: state.persons.concat( newPerson )
-            }
-        case actionTypes.REMOVE_PERSON:
-            return {
-                ...state,
-                persons: state.persons.filter(person => person.id !== action.personId)
-            }
+const reducer = (state = initialState, action) => {
+    if (action.type === 'INCREMENT') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'DECREMENT') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+    if (action.type === 'ADD') {
+        return {
+            counter: state.counter + 10
+        }
+    }
+    if (action.type === 'SUBTRACT') {
+        return {
+            counter: state.counter - 8
+        }
     }
     return state;
 };
