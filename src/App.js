c61a85fe7e1497b8711bb10db723fb13dbb3d11b
Marko <29685366+MarkoTON@users.noreply.github.com>
2021-04-16 14:33:34 +0200

    useeffect cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Ingredients from './components/Ingredients/Ingredients';
+import Todo from './components/Todo';
 
-const App = props => {
-  return <Ingredients />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Todo />
+      </div>
+    );
+  }
+}
 
 export default App;
