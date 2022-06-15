d9abb9115b8e981f1a020edf56904580451f8808
Marko_TON <markotoncanovic@gmail.com>
2022-06-15 19:12:15 +0200

    cmp-deep-dive-02-updating-lifecycle-props

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,55 +5,38 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
-  constructor( props ) {
-    super( props );
-    console.log( '[App.js] Inside Constructor', props );
-    this.state = {
-      persons: [
-        { id: 'asfa1', name: 'Max', age: 28 },
-        { id: 'vasdf1', name: 'Manu', age: 29 },
-        { id: 'asdf11', name: 'Stephanie', age: 26 }
-      ],
-      otherState: 'some other value',
-      showPersons: false
-    };
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
   }
 
-  componentWillMount () {
-    console.log( '[App.js] Inside componentWillMount()' );
+  state = {
+    persons: [
+      { id: 'asfa1', name: 'Max', age: 28 },
+      { id: 'vasdf1', name: 'Manu', age: 29 },
+      { id: 'asdf11', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
   }
 
-  componentDidMount () {
-    console.log( '[App.js] Inside componentDidMount()' );
-  }
-
-  shouldComponentUpdate ( nextProps, nextState ) {
-    console.log( '[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState );
-    return true;
-  }
-
-  componentWillUpdate ( nextProps, nextState ) {
-    console.log( '[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState );
-  }
-
-  componentDidUpdate () {
-    console.log( '[UPDATE App.js] Inside componentDidUpdate' );
-  }
-
-  // state = {
-  //   persons: [
-  //     { id: 'asfa1', name: 'Max', age: 28 },
-  //     { id: 'vasdf1', name: 'Manu', age: 29 },
-  //     { id: 'asdf11', name: 'Stephanie', age: 26 }
-  //   ],
-  //   otherState: 'some other value',
-  //   showPersons: false
+  // componentWillMount() {
+  //   console.log('[App.js] componentWillMount');
   // }
 
-  nameChangedHandler = ( event, id ) => {
-    const personIndex = this.state.persons.findIndex( p => {
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
-    } );
+    });
 
     const person = {
       ...this.state.persons[personIndex]
@@ -66,39 +49,43 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState( { persons: persons } );
-  }
+    this.setState({ persons: persons });
+  };
 
-  deletePersonHandler = ( personIndex ) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
-    persons.splice( personIndex, 1 );
-    this.setState( { persons: persons } );
-  }
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState( { showPersons: !doesShow } );
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
-  render () {
-    console.log( '[App.js] Inside render()' );
+  render() {
+    console.log('[App.js] render');
     let persons = null;
 
-    if ( this.state.showPersons ) {
-      persons = <Persons
-        persons={this.state.persons}
-        clicked={this.deletePersonHandler}
-        changed={this.nameChangedHandler} />;
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
     }
 
     return (
       <div className={classes.App}>
         <Cockpit
-          appTitle={this.props.title}
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          clicked={this.togglePersonsHandler} />
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
