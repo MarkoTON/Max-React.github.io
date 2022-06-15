1683cf6fba10bb98f265f4789f50d8772585b632
Marko_TON <markotoncanovic@gmail.com>
2022-06-15 19:17:41 +0200

    cmp-deep-dive-05-shouldcomponentupdate-memo

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,52 +1,59 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person';
 
-class Persons extends PureComponent {
-    constructor( props ) {
-        super( props );
-        console.log( '[Persons.js] Inside Constructor', props );
-    }
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
 
-    componentWillMount () {
-        console.log( '[Persons.js] Inside componentWillMount()' );
-    }
+  // componentWillReceiveProps(props) {
+  //   console.log('[Persons.js] componentWillReceiveProps', props);
+  // }
 
-    componentDidMount () {
-        console.log( '[Persons.js] Inside componentDidMount()' );
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
     }
+    // return true;
+  }
 
-    componentWillReceiveProps ( nextProps ) {
-        console.log( '[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps );
-    }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!' };
+  }
 
-    // shouldComponentUpdate ( nextProps, nextState ) {
-    //     console.log( '[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState );
-    //     return nextProps.persons !== this.props.persons ||
-    //         nextProps.changed !== this.props.changed ||
-    //         nextProps.clicked !== this.props.clicked;
-    //     // return true;
-    // }
+  // componentWillUpdate() {
 
-    componentWillUpdate ( nextProps, nextState ) {
-        console.log( '[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState );
-    }
+  // }
 
-    componentDidUpdate () {
-        console.log( '[UPDATE Persons.js] Inside componentDidUpdate' );
-    }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
 
-    render () {
-        console.log( '[Persons.js] Inside render()' );
-        return this.props.persons.map( ( person, index ) => {
-            return <Person
-                click={() => this.props.clicked( index )}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={( event ) => this.props.changed( event, person.id )} />
-        } );
-    }
+  componentWillUnmount() {
+    console.log('[Persons.js] componentWillUnmount');
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
 }
 
 export default Persons;
