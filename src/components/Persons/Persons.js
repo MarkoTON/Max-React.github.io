2a04c89db35cbf66de5ffbcf847622f67690a60d
Marko_TON <markotoncanovic@gmail.com>
2022-06-15 19:21:11 +0200

    cmp-deep-dive--08-react-finished

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,61 +3,54 @@ import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
 class Persons extends PureComponent {
-  // static getDerivedStateFromProps(props, state) {
-  //   console.log('[Persons.js] getDerivedStateFromProps');
-  //   return state;
-  // }
+    constructor( props ) {
+        super( props );
+        console.log( '[Persons.js] Inside Constructor', props );
+        this.lastPersonRef = React.createRef();
+    }
 
-  // componentWillReceiveProps(props) {
-  //   console.log('[Persons.js] componentWillReceiveProps', props);
-  // }
+    componentWillMount () {
+        console.log( '[Persons.js] Inside componentWillMount()' );
+    }
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log('[Persons.js] shouldComponentUpdate');
-  //   if (
-  //     nextProps.persons !== this.props.persons ||
-  //     nextProps.changed !== this.props.changed ||
-  //     nextProps.clicked !== this.props.clicked
-  //   ) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  //   // return true;
-  // }
+    componentDidMount () {
+        console.log( '[Persons.js] Inside componentDidMount()' );
+        this.lastPersonRef.current.focus();
+    }
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('[Persons.js] getSnapshotBeforeUpdate');
-    return { message: 'Snapshot!' };
-  }
+    componentWillReceiveProps ( nextProps ) {
+        console.log( '[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps );
+    }
 
-  // componentWillUpdate() {
+    // shouldComponentUpdate ( nextProps, nextState ) {
+    //     console.log( '[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState );
+    //     return nextProps.persons !== this.props.persons ||
+    //         nextProps.changed !== this.props.changed ||
+    //         nextProps.clicked !== this.props.clicked;
+    //     // return true;
+    // }
 
-  // }
+    componentWillUpdate ( nextProps, nextState ) {
+        console.log( '[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState );
+    }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('[Persons.js] componentDidUpdate');
-    console.log(snapshot);
-  }
+    componentDidUpdate () {
+        console.log( '[UPDATE Persons.js] Inside componentDidUpdate' );
+    }
 
-  componentWillUnmount() {
-    console.log('[Persons.js] componentWillUnmount');
-  }
-
-  render() {
-    console.log('[Persons.js] rendering...');
-    return this.props.persons.map((person, index) => {
-      return (
-        <Person
-          click={() => this.props.clicked(index)}
-          name={person.name}
-          age={person.age}
-          key={person.id}
-          changed={event => this.props.changed(event, person.id)}
-        />
-      );
-    });
-  }
+    render () {
+        console.log( '[Persons.js] Inside render()' );
+        return this.props.persons.map( ( person, index ) => {
+            return <Person
+                click={() => this.props.clicked( index )}
+                name={person.name}
+                position={index}
+                age={person.age}
+                ref={this.lastPersonRef}
+                key={person.id}
+                changed={( event ) => this.props.changed( event, person.id )} />
+        } );
+    }
 }
 
 export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,81 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from "react";
 
-import classes from './App.css';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-import withClass from '../hoc/withClass';
-import Aux from '../hoc/Aux';
+import classes from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+import Aux from "../hoc/Aux";
+import withClass from "../hoc/WithClass";
 
-class App extends Component {
+export const AuthContext = React.createContext(false);
+
+class App extends PureComponent {
   constructor(props) {
     super(props);
-    console.log('[App.js] constructor');
+    console.log("[App.js] Inside Constructor", props);
+    this.state = {
+      persons: [
+        { id: "asfa1", name: "Max", age: 28 },
+        { id: "vasdf1", name: "Manu", age: 29 },
+        { id: "asdf11", name: "Stephanie", age: 26 }
+      ],
+      otherState: "some other value",
+      showPersons: false,
+      toggleClicked: 0,
+      authenticated: false
+    };
   }
 
-  state = {
-    persons: [
-      { id: 'asfa1', name: 'Max', age: 28 },
-      { id: 'vasdf1', name: 'Manu', age: 29 },
-      { id: 'asdf11', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false,
-    showCockpit: true
-  };
-
-  static getDerivedStateFromProps(props, state) {
-    console.log('[App.js] getDerivedStateFromProps', props);
-    return state;
+  componentWillMount() {
+    console.log("[App.js] Inside componentWillMount()");
   }
-
-  // componentWillMount() {
-  //   console.log('[App.js] componentWillMount');
-  // }
 
   componentDidMount() {
-    console.log('[App.js] componentDidMount');
+    console.log("[App.js] Inside componentDidMount()");
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[App.js] shouldComponentUpdate');
-    return true;
+  // shouldComponentUpdate ( nextProps, nextState ) {
+  //   console.log( '[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState );
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;
+  // }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log(
+      "[UPDATE App.js] Inside componentWillUpdate",
+      nextProps,
+      nextState
+    );
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log(
+      "[UPDATE App.js] Inside getDerivedStateFromProps",
+      nextProps,
+      prevState
+    );
+
+    return prevState;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log(
+      "[UPDATE App.js] Inside getSnapshotBeforeUpdate"
+    );
   }
 
   componentDidUpdate() {
-    console.log('[App.js] componentDidUpdate');
+    console.log("[UPDATE App.js] Inside componentDidUpdate");
   }
+
+  // state = {
+  //   persons: [
+  //     { id: 'asfa1', name: 'Max', age: 28 },
+  //     { id: 'vasdf1', name: 'Manu', age: 29 },
+  //     { id: 'asdf11', name: 'Stephanie', age: 26 }
+  //   ],
+  //   otherState: 'some other value',
+  //   showPersons: false
+  // }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -73,11 +105,20 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      };
+    });
+  };
+
+  loginHandler = () => {
+    this.setState({ authenticated: true });
   };
 
   render() {
-    console.log('[App.js] render');
+    console.log("[App.js] Inside render()");
     let persons = null;
 
     if (this.state.showPersons) {
@@ -94,20 +135,21 @@ class App extends Component {
       <Aux>
         <button
           onClick={() => {
-            this.setState({ showCockpit: false });
+            this.setState({ showPersons: true });
           }}
         >
-          Remove Cockpit
+          Show Persons
         </button>
-        {this.state.showCockpit ? (
-          <Cockpit
-            title={this.props.appTitle}
-            showPersons={this.state.showPersons}
-            personsLength={this.state.persons.length}
-            clicked={this.togglePersonsHandler}
-          />
-        ) : null}
-        {persons}
+        <Cockpit
+          appTitle={this.props.title}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          login={this.loginHandler}
+          clicked={this.togglePersonsHandler}
+        />
+        <AuthContext.Provider value={this.state.authenticated}>
+          {persons}
+        </AuthContext.Provider>
       </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
